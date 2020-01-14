e4b094faa0f1856956e09248938c97d3839c05e1
remuty <remuty.work@gmail.com>
2020-01-15 00:40:22 +0900

    add 新規登録画面からユーザー登録

--- Twiiiiiter/Controller/RegisterViewController.swift
+++ Twiiiiiter/Controller/RegisterViewController.swift
@@ -8,23 +8,39 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
-
+class RegisterViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTextField: RoundedTextField!
+    @IBOutlet weak var passwordTextField: RoundedTextField!
+    @IBOutlet weak var passwordConfirmTextField: RoundedTextField!
+    @IBOutlet weak var nameTextField: RoundedTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordConfirmTextField.delegate = self
+        nameTextField.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-    */
-
+    
+    @IBAction func registerAction(_ sender: Any) {
+        API.register(name: emailTextField.text!, email: emailTextField.text!, password: passwordTextField.text!, password_confirm: passwordConfirmTextField.text!)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

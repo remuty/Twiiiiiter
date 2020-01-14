d70138dce21ffaa90c529420200707a8a33c9125
remuty <remuty.work@gmail.com>
2020-01-15 00:19:08 +0900

    add ログイン画面　新規登録画面追加

---
+++ Twiiiiiter/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Twiiiiiter
+//
+//  Created by remuty on 2020/01/15.
+//  Copyright © 2020 remuty. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
